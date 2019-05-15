f1e7c5d4545e629adaf073006b056dfe5273f9db
novelia <novelia>
2019-05-15 14:19:04 +0700

    Passess CD 10 & CD 17 Learning Objective

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var
+    learnerGenderLabel: UILabel!
+    
+    @IBOutlet weak var
+    learnerAgeLabel: UILabel!
+    
+    @IBOutlet weak var
+    learnerNameLabel: UILabel!
+    
+    var learnerInstance: LearnerModel? // You just create copy of your LearnerModel
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        learnerInstance = LearnerModel(nameLearner: "Novelia", ageLearner: 20, genderLearner: "Female", imageProfileLearner: "")
+        updateUI()
+        }
+    
+    func updateUI() {
+        if let instance = learnerInstance { learnerNameLabel.text = instance.name
+        learnerAgeLabel.text = "\(instance.age)"
+        learnerGenderLabel.text = instance.gender
+
     }
-
-
+    
+    
+    
 }
-
+    @IBAction func increaseAgeTapped(_ sender: UIButton) {
+        if let instance = learnerInstance { instance.increaseAge()
+            updateUI()
+        }
+    }
+    
+}
